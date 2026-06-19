bd34cff50f0683f97345648d2c7799c7aec1463f
kurumi <kurumihere@disroot.org>
2026-06-19 14:34:31 +0500

    Update license header in `aecat.c`

--- aecat.c
+++ aecat.c
@@ -1,17 +1,8 @@
-/*
- * Copyright (c) 2026, Ae-foundation. All rights reserved.
+/* SPDX-License-Identifier: BSD-2-Clause
  *
- * Permission to use, copy, modify, and/or distribute this software for
- * any purpose with or without fee is hereby granted, provided that the
- * software name includes “ae”.
+ * Copyright (c) 2026, kurumihere
  *
- * THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL
- * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
- * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
- * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
- * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * See LICENSE for details.
  */
 
 #include <errno.h>
